62908af3f63816cff907f5ed46ccd19ab68e8b84
Trainmaster2 <dphammock@gmail.com>
2016-12-13 17:09:11 -0800

    Update Tic-Tac-Toe.bat

--- Tic-Tac-Toe.bat
+++ Tic-Tac-Toe.bat
@@ -22,3 +22,32 @@ echo (1) Player
 echo (2) CPU
 choice /c 12 /n
 set P1=%errorlevel%
+
+set P2=1
+goto verify
+
+cls
+echo Is Player 2 a...
+echo.
+echo (1) Player
+echo (2) CPU
+choice /c 12 /n
+set P2=%errorlevel%
+
+:verify
+cls
+if "%P1%"=="1" (echo Player 1 is a Player)
+if "%P1%"=="2" (echo Player 1 is a CPU)
+if "%P2%"=="1" (echo Player 2 is a Player)
+if "%P2%"=="2" (echo Player 2 is a CPU)
+echo.
+choice /c yn /n /m Is this correct? (Y/N)
+
+:display
+echo.
+echo.
+echo   %a1%║%b1%║%c1%
+echo   ═╬═╬═
+echo   %a2%║%b2%║%c2%
+echo   ═╬═╬═
+echo   %a3%║%b3%║%c3%
